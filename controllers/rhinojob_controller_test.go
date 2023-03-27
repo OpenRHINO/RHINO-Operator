ebdefa10afa1758de91882cf78f40039cb77bc63
avalanche <anwentao1@gmail.com>
2023-03-27 14:27:41 +0800

    fix Unit test code need to be renewed #34 change field `Name` of namespaced launcher and workers after applying unique job names for k8s-jobs

--- controllers/rhinojob_controller_test.go
+++ controllers/rhinojob_controller_test.go
@@ -48,8 +48,6 @@ var _ = Describe("RhinoJob controller", func() {
 		}
 
 		namespacedName := types.NamespacedName{Name: RhinoJobName, Namespace: RhinoJobName}
-		namespacedNameLauncher := types.NamespacedName{Name: RhinoJobName + "-launcher", Namespace: RhinoJobName}
-		namespacedNameWorkers := types.NamespacedName{Name: RhinoJobName + "-workers", Namespace: RhinoJobName}
 
 		BeforeEach(func() {
 			By("Creating the Namespace to perform the tests")
@@ -98,18 +96,26 @@ var _ = Describe("RhinoJob controller", func() {
 				Expect(err).To(Not(HaveOccurred()))
 			}
 
+			createdRhinojob := &rhinooprapiv1alpha1.RhinoJob{}
 			By("Checking if the custom resource was successfully created")
 			Eventually(func() error {
-				found := &rhinooprapiv1alpha1.RhinoJob{}
-				return k8sClient.Get(ctx, namespacedName, found)
+				return k8sClient.Get(ctx, namespacedName, createdRhinojob)
 			}, time.Minute, time.Second).Should(Succeed())
 
+			namespacedNameLauncher := types.NamespacedName{
+				Name:      nameForLauncherJob(createdRhinojob),
+				Namespace: RhinoJobName,
+			}
 			By("Checking if the launcher job was successfully created in the reconciliation")
 			Eventually(func() error {
 				found := &kbatchv1.Job{}
 				return k8sClient.Get(ctx, namespacedNameLauncher, found)
 			}, time.Minute, time.Second).Should(Succeed())
 
+			namespacedNameWorkers := types.NamespacedName{
+				Name:      nameForWorkersJob(createdRhinojob),
+				Namespace: RhinoJobName,
+			}
 			By("Checking if the workers job was successfully created in the reconciliation")
 			Eventually(func() error {
 				found := &kbatchv1.Job{}
